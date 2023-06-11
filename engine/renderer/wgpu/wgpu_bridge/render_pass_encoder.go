ca6a7629f7bf7487069c7f350656839a1b2a1955
uglyer <root@wanda.sb>
2023-06-11 20:36:29 +0800

    🚧 补全 RenderPassEncoder 接口

--- engine/renderer/wgpu/wgpu_bridge/render_pass_encoder.go
+++ engine/renderer/wgpu/wgpu_bridge/render_pass_encoder.go
@@ -27,7 +27,29 @@ type RenderPassDescriptor struct {
 }
 
 type RenderPassEncoder interface {
-	Drop()
+	IDrop
+	Draw(vertexCount, instanceCount, firstVertex, firstInstance uint32)
+	DrawIndexed(indexCount, instanceCount, firstIndex uint32, baseVertex int32, firstInstance uint32)
+	DrawIndexedIndirect(indirectBuffer IGPUBuffer, indirectOffset uint64)
+	DrawIndirect(indirectBuffer IGPUBuffer, indirectOffset uint64)
+	End()
+	ExecuteBundles(bundles ...IGPURenderBundle)
+	InsertDebugMarker(markerLabel string)
+	PopDebugGroup()
+	PushDebugGroup(groupLabel string)
+	SetBindGroup(groupIndex uint32, group IGPUBindGroup, dynamicOffsets []uint32)
+	SetBlendConstant(color *Color)
+	SetIndexBuffer(buffer IGPUBuffer, format IndexFormat, offset, size uint64)
+	SetPipeline(pipeline IGPURenderPipeLine)
+	SetScissorRect(x, y, width, height uint32)
+	SetStencilReference(reference uint32)
+	SetVertexBuffer(slot uint32, buffer IGPUBuffer, offset, size uint64)
+	SetViewport(x, y, width, height, minDepth, maxDepth float32)
+	SetPushConstants(stages ShaderStage, offset uint32, data []byte)
+	MultiDrawIndirect(encoder *RenderPassEncoder, buffer IGPUBuffer, offset uint64, count uint32)
+	MultiDrawIndexedIndirect(encoder *RenderPassEncoder, buffer IGPUBuffer, offset uint64, count uint32)
+	MultiDrawIndirectCount(encoder *RenderPassEncoder, buffer IGPUBuffer, offset uint64, countBuffer IGPUBuffer, countBufferOffset uint64, maxCount uint32)
+	MultiDrawIndexedIndirectCount(encoder *RenderPassEncoder, buffer IGPUBuffer, offset uint64, countBuffer IGPUBuffer, countBufferOffset uint64, maxCount uint32)
 }
 
 type CommandBufferDescriptor struct {
