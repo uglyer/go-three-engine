dc5e878fcb264b71a79d71d456bfae81ce1ac26b
uglyer <root@wanda.sb>
2023-06-17 23:09:39 +0800

    ⚗️ TODO 使用反射实现类型映射转换

--- engine/renderer/wgpu/wgpu_wasm/adapter.go
+++ engine/renderer/wgpu/wgpu_wasm/adapter.go
@@ -7,6 +7,8 @@ import (
 	"github.com/uglyer/go-three-engine/engine/renderer/wgpu"
 	"github.com/uglyer/go-three-engine/engine/wasm"
 	"log"
+	"reflect"
+	"strings"
 	"syscall/js"
 )
 
@@ -42,8 +44,58 @@ func (a *Adapter) EnumerateFeatures() []wgpu.FeatureName {
 }
 func (a *Adapter) GetLimits() wgpu.SupportedLimits {
 	// TODO impl adapter EnumerateFeatures
-	wasm.ConsoleLog("device.GetLimits", a.ref.Get("limits"))
-	return wgpu.SupportedLimits{}
+	// TODO 使用反射实现类型映射转换
+	//jsLimits := a.ref.Get("limits")
+	limit := wgpu.Limits{}
+	valueOfLimit := reflect.ValueOf(&limit).Elem()
+	typeOfLimit := reflect.TypeOf(limit)
+	wasm.ConsoleLog("valueOfLimit.Kind().String()", valueOfLimit.Kind().String())
+	size := typeOfLimit.NumField()
+	for i := 0; i < size; i++ {
+		name := typeOfLimit.Field(i).Name
+		field := valueOfLimit.FieldByName(name)
+		name = strings.ToLower(name[0:1]) + name[1:]
+		wasm.ConsoleLog("name", name)
+		field.SetUint(1)
+		//field.SetUint(uint64(jsLimits.Get(name).Int()))
+	}
+	//limits.Get("maxBindGroups")
+	wasm.ConsoleLog("device.GetLimits", limit)
+	return wgpu.SupportedLimits{
+		Limits: &wgpu.Limits{
+			//MaxTextureDimension1D:                     uint32(limits.Get("maxTextureDimension1D").Int()),
+			//MaxTextureDimension2D:                     uint32(limits.Get("maxTextureDimension1D").Int()),
+			//MaxTextureDimension3D:                     uint32(limits.Get("maxTextureDimension1D").Int()),
+			//MaxTextureArrayLayers:                     uint32(limits.Get("maxTextureDimension1D").Int()),
+			//MaxBindGroups:                             uint32(limits.Get("maxTextureDimension1D").Int()),
+			//MaxBindingsPerBindGroup:                   uint32(limits.Get("maxTextureDimension1D").Int()),
+			//MaxDynamicUniformBuffersPerPipelineLayout: uint32(limits.Get("maxTextureDimension1D").Int()),
+			//MaxDynamicStorageBuffersPerPipelineLayout: uint32(limits.Get("maxTextureDimension1D").Int()),
+			//MaxSampledTexturesPerShaderStage:          uint32(limits.Get("maxTextureDimension1D").Int()),
+			//MaxSamplersPerShaderStage:                 uint32(limits.Get("maxTextureDimension1D").Int()),
+			//MaxStorageBuffersPerShaderStage:           uint32(limits.Get("maxTextureDimension1D").Int()),
+			//MaxStorageTexturesPerShaderStage:          uint32(limits.Get("maxTextureDimension1D").Int()),
+			//MaxUniformBuffersPerShaderStage:           uint32(limits.Get("maxTextureDimension1D").Int()),
+			//MaxUniformBufferBindingSize:               uint64(limits.Get("maxTextureDimension1D").Int()),
+			//MaxStorageBufferBindingSize:               uint64(limits.Get("maxTextureDimension1D").Int()),
+			//MinUniformBufferOffsetAlignment:           uint32(limits.Get("maxTextureDimension1D").Int()),
+			//MinStorageBufferOffsetAlignment:           uint32(limits.Get("maxTextureDimension1D").Int()),
+			//MaxVertexBuffers:                          uint32(limits.Get("maxTextureDimension1D").Int()),
+			//MaxBufferSize:                             uint32(limits.Get("maxTextureDimension1D").Int()),
+			//MaxVertexAttributes:                       uint32(limits.Get("maxTextureDimension1D").Int()),
+			//MaxVertexBufferArrayStride:                uint32(limits.Get("maxTextureDimension1D").Int()),
+			//MaxInterStageShaderComponents:             uint32(limits.Get("maxTextureDimension1D").Int()),
+			//MaxInterStageShaderVariables:              uint32(limits.Get("maxTextureDimension1D").Int()),
+			//MaxColorAttachments:                       uint32(limits.Get("maxTextureDimension1D").Int()),
+			//MaxComputeWorkgroupStorageSize:            uint32(limits.Get("maxTextureDimension1D").Int()),
+			//MaxComputeInvocationsPerWorkgroup:         uint32(limits.Get("maxTextureDimension1D").Int()),
+			//MaxComputeWorkgroupSizeX:                  uint32(limits.Get("maxTextureDimension1D").Int()),
+			//MaxComputeWorkgroupSizeY:                  0,
+			//MaxComputeWorkgroupSizeZ:                  0,
+			//MaxComputeWorkgroupsPerDimension:          0,
+			//MaxPushConstantSize:                       0,
+		},
+	}
 }
 func (a *Adapter) GetProperties() wgpu.AdapterProperties {
 	// TODO impl adapter EnumerateFeatures
