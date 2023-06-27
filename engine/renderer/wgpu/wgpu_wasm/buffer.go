ae3bbcbcf2909b6c58cb5e0fcf293f4f3dccd832
uglyer <root@wanda.sb>
2023-06-27 22:09:51 +0800

    🚧 实现 Buffer.MapAsync

--- engine/renderer/wgpu/wgpu_wasm/buffer.go
+++ engine/renderer/wgpu/wgpu_wasm/buffer.go
@@ -2,6 +2,7 @@ package wgpu_wasm
 
 import (
 	"github.com/uglyer/go-three-engine/engine/renderer/wgpu"
+	"github.com/uglyer/go-three-engine/engine/wasm"
 	"syscall/js"
 )
 
@@ -17,8 +18,17 @@ func (b *Buffer) Destroy() {
 	panic("todo impl Destroy")
 }
 
-func (b *Buffer) MapAsync(mode wgpu.MapMode, offset uint64, size uint64, callback wgpu.BufferMapCallback){
-	panic("todo impl MapAsync")
+func (b *Buffer) MapAsync(mode wgpu.MapMode, offset uint64, size uint64, callback wgpu.BufferMapCallback) {
+	promise := b.ref.Call("mapAsync", int(mode), offset, size)
+	go func() {
+		_, err := wasm.Await(promise)
+		if err != nil {
+			wasm.ConsoleError("MapAsync", err.Error())
+			callback(wgpu.BufferMapAsyncStatus_Error)
+			return
+		}
+		callback(wgpu.BufferMapAsyncStatus_Success)
+	}()
 }
 
 func (b *Buffer) GetMappedRange(offset, size uint) []byte {
