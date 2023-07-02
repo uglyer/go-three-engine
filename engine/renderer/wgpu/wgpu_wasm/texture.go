29b845b81a9c7df1a15abad4e9e4a6a3e897fbad
uglyer <root@wanda.sb>
2023-07-02 21:33:27 +0800

    🚧 实现 Texture.CreateView

--- engine/renderer/wgpu/wgpu_wasm/texture.go
+++ engine/renderer/wgpu/wgpu_wasm/texture.go
@@ -15,7 +15,33 @@ func newTexture(ref js.Value) wgpu.ITexture {
 }
 
 func (p *Texture) CreateView(descriptor *wgpu.TextureViewDescriptor) wgpu.ITextureView {
-	panic("todo impl CreateView")
+	if descriptor == nil {
+		ref := p.ref.Call("createView")
+		return &TextureView{ref: ref}
+	}
+	desc := map[string]any{
+		"label": descriptor.Label,
+	}
+	if &descriptor.ArrayLayerCount != nil {
+		desc["arrayLayerCount"] = descriptor.ArrayLayerCount
+	}
+	if &descriptor.Aspect != nil {
+		desc["aspect"] = descriptor.Aspect.String()
+	}
+	if &descriptor.BaseArrayLayer != nil {
+		desc["baseArrayLayer"] = descriptor.BaseArrayLayer
+	}
+	if &descriptor.BaseMipLevel != nil {
+		desc["baseMipLevel"] = descriptor.BaseMipLevel
+	}
+	if &descriptor.Dimension != nil {
+		desc["dimension"] = descriptor.Dimension.String()
+	}
+	if &descriptor.Format != nil {
+		desc["format"] = descriptor.Format.String()
+	}
+	ref := p.ref.Call("createView", desc)
+	return &TextureView{ref: ref}
 }
 
 func (p *Texture) Destroy() {
