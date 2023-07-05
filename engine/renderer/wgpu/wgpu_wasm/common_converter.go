4bc7fc26d881ce62e9fee4ab81da37ac35d1cd01
uglyer <root@wanda.sb>
2023-07-05 23:02:09 +0800

    🐛 修复数组转换类型错误的问题

--- engine/renderer/wgpu/wgpu_wasm/common_converter.go
+++ engine/renderer/wgpu/wgpu_wasm/common_converter.go
@@ -1,17 +1,21 @@
 package wgpu_wasm
 
-import "github.com/uglyer/go-three-engine/engine/renderer/wgpu"
+import (
+	"github.com/uglyer/go-three-engine/engine/renderer/wgpu"
+	"github.com/uglyer/go-three-engine/engine/wasm"
+	"syscall/js"
+)
 
-func ConvertOrigin3DToArray(point *wgpu.Origin3D) []uint32 {
-	return []uint32{point.X, point.Y, point.Z}
+func ConvertOrigin3DToArray(point *wgpu.Origin3D) js.Value {
+	return wasm.NewArray([]uint32{point.X, point.Y, point.Z})
 }
 
-func ConvertExtends3DToArray(point *wgpu.Extent3D) []uint32 {
-	return []uint32{point.Width, point.Height, point.DepthOrArrayLayers}
+func ConvertExtends3DToArray(point *wgpu.Extent3D) js.Value {
+	return wasm.NewArray([]uint32{point.Width, point.Height, point.DepthOrArrayLayers})
 }
 
-func ConvertColorToArray(point *wgpu.Color) []float64 {
-	return []float64{point.R, point.G, point.B, point.A}
+func ConvertColorToArray(point *wgpu.Color) js.Value {
+	return wasm.NewArray([]float64{point.R, point.G, point.B, point.A})
 }
 
 func ConvertImageCopyBufferToMap(source *wgpu.ImageCopyBuffer) map[string]any {
