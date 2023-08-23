0633cdf2b0516b931f1bfd806fc48295055913f4
uglyer <root@wanda.sb>
2023-08-23 21:49:48 +0800

    🚧 新增属性拷贝顶点到三维向量中

--- engine/core/buffer_attribute.go
+++ engine/core/buffer_attribute.go
@@ -1,5 +1,7 @@
 package core
 
+import "github.com/uglyer/go-three-engine/engine/math64"
+
 // BufferAttribute 几何体属性数据
 type BufferAttribute struct {
 	// Array
@@ -8,4 +10,9 @@ type BufferAttribute struct {
 	ItemSize int
 	// Count 成员个数 (Array/ItemSize)
 	Count int
+}
+
+// CopyToVector3 拷贝顶点到三维向量中
+func (b *BufferAttribute) CopyToVector3(v *math64.Vector3, index int) *math64.Vector3 {
+	return v.Set(b.Array[index*b.ItemSize], b.Array[index*b.ItemSize+1], b.Array[index*b.ItemSize+2])
 }
