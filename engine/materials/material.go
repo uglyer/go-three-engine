4ddee31c6a1707eb8b7b64ee2bf8354dba0b13e6
uglyer <root@wanda.sb>
2023-11-08 21:40:42 +0800

    🚧 定义材质数据结构

--- engine/materials/material.go
+++ engine/materials/material.go
@@ -40,4 +40,35 @@ const (
 
 type Material struct {
 	core.EventDispatcher
+	refCount int // Current number of references
+
+	// Shader specification
+	shader       string // Shader name
+	shaderUnique bool   // shader has only one instance (does not depend on lights or textures)
+	//ShaderDefines gls.ShaderDefines // shader defines
+
+	side        Side      // Face side(s) visibility
+	blending    Blending  // Blending mode
+	useLights   UseLights // Which light types to consider
+	transparent bool      // Whether at all transparent
+	wireframe   bool      // Whether to render only the wireframe
+	lineWidth   float32   // Line width for lines and wireframe
+	//textures    []*texture.Texture2D // List of textures
+
+	polyOffsetFactor float32 // polygon offset factor
+	polyOffsetUnits  float32 // polygon offset units
+
+	depthMask bool   // Enable writing into the depth buffer
+	depthTest bool   // Enable depth buffer test
+	depthFunc uint32 // Active depth test function
+
+	// TODO stencil properties
+
+	// Equations used for custom blending (when blending=BlendCustom) // TODO implement methods
+	blendRGB      uint32 // separate blending equation for RGB
+	blendAlpha    uint32 // separate blending equation for Alpha
+	blendSrcRGB   uint32 // separate blending func source RGB
+	blendDstRGB   uint32 // separate blending func dest RGB
+	blendSrcAlpha uint32 // separate blending func source Alpha
+	blendDstAlpha uint32 // separate blending func dest Alpha
 }
